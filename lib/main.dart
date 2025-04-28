9418714f7718fc7446d6bc06648a7f94638c04ae
Anıl Muslu <anillmusluu@gmail.com>
2025-04-28 11:24:51 +0300

    Initial commit: Flutter Hello GitHub App

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello GitHub',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello GitHub!'),
+      ),
+      body: const Center(
+        child: Text(
+          'Bu benim GitHub\'a yüklediğim ilk Flutter projem!',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
